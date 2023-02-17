ebdd1b4df55a89a6889d719e0b2b71f6986d71ff
Just-Midhun <113001139+Just-Midhun@users.noreply.github.com>
2023-02-17 21:29:35 +0530

    Add files via upload

---
+++ 5.simple chrome extension/scripts/main.js
@@ -0,0 +1,50 @@
+let urlSet=[];
+
+let inputTaken=document.getElementById("input-el");
+let output=document.getElementById("savedValues");
+let saved=document.getElementById("save");
+let clear=document.getElementById("clear");
+
+let urlFromLocalStorage=JSON.parse(localStorage.getItem("urlSet"));//get items stored in local storage
+
+if(urlFromLocalStorage){
+    urlSet=urlFromLocalStorage;
+    renderValue();
+}
+
+saved.addEventListener("click", saveUrl);
+clear.addEventListener("click", clearUrl);
+
+function saveUrl() {
+
+    urlSet.push(inputTaken.value); //.values used to get input values
+    inputTaken.value = "";//used to clear the input prompt
+    localStorage.setItem("urlSet", JSON.stringify(urlSet));//storing values to local storage on site by saving array in string format 
+    renderValue();
+}
+
+function renderValue() {
+
+    let listItems="";
+    for(let i = 0; i < urlSet.length; i++) {
+        
+        listItems +="<li><a target='_blank' href='"+urlSet[i]+"'>"+ urlSet[i] + "</a></li>"; //used to create a tag to target link and open on new tab by blanktarget
+        /*can also be written as :
+        `
+        <li>
+            <a target="_blank" href='${urlSet[i]}'>
+                ${urlSet[i]}
+            </a>
+        </li>
+        `
+        */
+    }
+    output.innerHTML=listItems;//innerHTML used to render html stuff in js
+
+}
+
+function clearUrl(){
+    localStorage.removeItem('urlSet');
+    output.innerHTML="";
+} 
+    
